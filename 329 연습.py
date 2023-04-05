fd0288e5149ec7e4789ce91973a73e0545baa163
sowon <sowon.park@stonybrook.edu>
2023-04-05 15:26:38 +0900

    finish

---
+++ 329 연습.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = True
+    if type(s) == int:
+        if (len(S) == 4 or 6):
+            return answer
+
+    else:
+        return not answer
+        
+
+print(solution("a234"))
+print(solution("1234"))
+print(solution("0101"))
